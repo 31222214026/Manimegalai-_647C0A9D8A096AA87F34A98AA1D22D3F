cf3bfa68822ba42108fb36e9acb03dcf86a68c3e
31222214026 <144304300+31222214026@users.noreply.github.com>
2023-09-22 10:06:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def fact_rec(n):
-   if n==0 or n==1:
-       return 1
-   else:
-       return n*fact_rec(n-1)
-
-number= int(input("Enter the value: "))
-result= fact_rec(number)
-print("The factorial of", number, "is", fact_rec(number))
+def linearSearchProduct(ProductList,targetproduct):
+   indices = []
+  
+   for index, product in enumerate(ProductList):
+      if product == targetproduct:
+       indices.append(index)
+   return indices
+#Example usage
+products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+target2 = "apple"
+result = linearSearchProduct(products,target)
+print(result)
